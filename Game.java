89d806dd92a8ca53b4afd68de4ac1283cfae9fd1
Alikina <alikina95@yandex.ru>
2015-05-20 08:46:25 +0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,15 +1,16 @@
-
 public class Game {
-	
-	
-	public void roll (int pins){
-	
-	}
-	
-	public int score() {
-	
-		return (0);
-	}
-	
+  private int rolls[] = new int[21];
+  private int currentRoll = 0;
+
+  public void roll(int pins) {
+    rolls[currentRoll++] = pins;
+  }
+
+  public int score() {
+    int score = 0;
+    for (int i = 0; i < rolls.length; i++)
+      score += rolls[i];
+    return score;
+  }
 }
 
