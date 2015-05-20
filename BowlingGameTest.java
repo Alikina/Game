7b7705c63fc3a41d81d778243b9b8b2a3b83867d
Alikina <alikina95@yandex.ru>
2015-05-20 08:47:15 +0500

    Update BowlingGameTest.java

--- BowlingGameTest.java
+++ BowlingGameTest.java
@@ -1,21 +1,32 @@
-//import static org.junit.Assert.*;
 import junit.framework.TestCase;
-//import org.junit.Test;
-
 
 public class BowlingGameTest extends TestCase {
-	public void testGutterGame() throws Exception{
-		Game g = new Game();
-		for (int i=0; i<20; i++)
-			g.roll(0);
-		assertEquals(0, g.score());
-	}
-	
-	public void testAllOnes() throws Exception{
-		Game g = new Game();
-		for (int i=0; i<20;i++)
-			g.roll(1);
-		assertEquals(20, g.score());
-	}
-}
+  private Game g;
 
+  protected void setUp() throws Exception {
+    g = new Game();
+  }
+
+  private void rollMany(int n, int pins) {
+    for (int i = 0; i < n; i++)
+      g.roll(pins);
+  }
+
+  public void testGutterGame() throws Exception {
+    rollMany(20, 0);
+    assertEquals(0, g.score());
+  }
+
+  public void testAllOnes() throws Exception {
+    rollMany(20,1);
+    assertEquals(20, g.score());
+  }
+
+  public void testOneSpare() throws Exception {
+    g.roll(5);
+    g.roll(5); // spare
+    g.roll(3);
+    rollMany(17,0);
+    assertEquals(16,g.score());
+  }
+}
